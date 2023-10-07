4c8db74812a79847a242d94a8477cfdc7c49b98a
Call Vin <kanggara75@gmail.com>
2023-10-08 00:09:52 +0700

    fix: Title bar

--- app/View/Components/AppLayout.php
+++ app/View/Components/AppLayout.php
@@ -8,6 +8,15 @@ use Illuminate\View\View;
 
 class AppLayout extends Component
 {
+    /**
+     * Create the component instance.
+     */
+    public $title;
+    public function __construct($title = null)
+    {
+        $this->title = $title;
+    }
+
     public function render(): View
     {
         $id = auth()->user()->id;
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,22 +1,14 @@
-<x-app-layout>
-  <div class="py-12">
-    <div class="mx-auto max-w-7xl space-y-6 sm:px-6 lg:px-8">
-      <div class="bg-white p-4 shadow dark:bg-gray-800 sm:rounded-lg sm:p-8">
-        <div class="max-w-xl">
-          <livewire:profile.update-profile-information-form />
-        </div>
+<x-app-layout title="Profile">
+  <div class="p-6 pb-24 sm:pt-12">
+    <div class="mx-auto max-w-3xl space-y-6 sm:px-6 lg:px-8">
+      <div class="rounded-lg bg-white p-4 shadow dark:bg-gray-800 sm:p-8">
+        <livewire:profile.update-profile-information-form />
       </div>
-
-      <div class="bg-white p-4 shadow dark:bg-gray-800 sm:rounded-lg sm:p-8">
-        <div class="max-w-xl">
-          <livewire:profile.update-password-form />
-        </div>
+      <div class="rounded-lg bg-white p-4 shadow dark:bg-gray-800 sm:p-8">
+        <livewire:profile.update-password-form />
       </div>
-
-      <div class="bg-white p-4 shadow dark:bg-gray-800 sm:rounded-lg sm:p-8">
-        <div class="max-w-xl">
-          <livewire:profile.delete-user-form />
-        </div>
+      <div class="rounded-lg bg-white p-4 shadow dark:bg-gray-800 sm:p-8">
+        <livewire:profile.delete-user-form />
       </div>
     </div>
   </div>
