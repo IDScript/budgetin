7c4b7dde6652febc68daa66718ad8f8f5cf949c7
Call Vin <kanggara75@gmail.com>
2023-10-07 21:35:18 +0700

    fix: Move /dashboard to /home

--- resources/views/livewire/welcome/navigation.blade.php
+++ resources/views/livewire/welcome/navigation.blade.php
@@ -1,7 +1,7 @@
 <div class="z-10 p-6 text-right sm:fixed sm:right-0 sm:top-0">
   @auth
     <a class="font-semibold text-gray-600 hover:text-sky-500 focus:rounded-sm focus:outline focus:outline-2 focus:outline-red-500 dark:text-sky-400 dark:hover:text-white"
-      href="{{ url("/dashboard") }}" wire:navigate>Dashboard</a>
+      href="{{ url("/home") }}" wire:navigate>Dashboard</a>
   @else
     <a class="font-semibold text-gray-600 hover:text-sky-500 focus:rounded-sm focus:outline focus:outline-2 focus:outline-red-500 dark:text-sky-400 dark:hover:text-white"
       href="{{ route("login") }}" wire:navigate>Log in</a>
