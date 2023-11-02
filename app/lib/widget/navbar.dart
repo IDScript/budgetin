46af76f6b2126af3cc7d9c7c80450ea7ee63086c
Call Vin <kanggara75@gmail.com>
2023-11-02 16:08:03 +0700

    add: NavBar init

---
+++ app/lib/widget/navbar.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+List<BottomNavigationBarItem> bottomNavItems = const <BottomNavigationBarItem>[
+  BottomNavigationBarItem(
+    icon: Icon(Icons.home_outlined),
+    label: 'Home',
+  ),
+  BottomNavigationBarItem(
+    icon: Icon(Icons.grid_3x3),
+    label: 'Category',
+  ),
+  BottomNavigationBarItem(
+    icon: Icon(Icons.search_outlined),
+    label: 'Search',
+  ),
+  BottomNavigationBarItem(
+    icon: Icon(Icons.favorite_outline),
+    label: 'Favourite',
+  ),
+  BottomNavigationBarItem(
+    icon: Icon(Icons.shopping_bag_outlined),
+    label: 'Cart',
+  ),
+];
+
+const List<Widget> bottomNavScreen = <Widget>[
+  Text('Index 0: Home'),
+  Text('Index 1: Category'),
+  Text('Index 2: Search'),
+  Text('Index 3: Favourite'),
+  Text('Index 4: Cart'),
+];
