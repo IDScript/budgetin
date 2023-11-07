220ed9c61d4415c40a06d3032324f4b14eb9f7ed
Call Vin <kanggara75@gmail.com>
2023-11-07 10:49:27 +0700

    add: Themes config

---
+++ app/lib/config/themes.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightTheme() {
+  return ThemeData(
+    fontFamily: 'Poppins',
+    brightness: Brightness.light,
+  );
+}
+
+ThemeData darkTheme() {
+  return ThemeData(
+    fontFamily: 'Poppins',
+    brightness: Brightness.dark,
+  );
+}
--- app/lib/main.dart
+++ app/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:window_size/window_size.dart';
 
 import 'config/routes.dart';
+import 'config/themes.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -21,6 +22,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: lightTheme(),
+      darkTheme: darkTheme(),
       debugShowCheckedModeBanner: false,
       initialRoute: '/',
       onGenerateRoute: RouteGenerator().generateRoute,
