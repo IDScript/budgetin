4000e29138bc6de6b75485125ec44d9d5b0316ad
Call Vin <kanggara75@gmail.com>
2023-11-25 07:57:52 +0700

    add: #app AuthenticationBloc

---
+++ app/lib/app.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:budgetin/ui/home_page.dart';
+import 'package:budgetin/config/routes.dart';
+import 'package:budgetin/config/themes.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:budgetin/login/view/login_page.dart';
+import 'package:user_repository/user_repository.dart';
+import 'package:budgetin/authentication/authentication.dart';
+import 'package:authentication_repository/authentication_repository.dart';
+
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  late final AuthenticationRepository _authenticationRepository;
+  late final UserRepository _userRepository;
+
+  @override
+  void initState() {
+    super.initState();
+    _authenticationRepository = AuthenticationRepository();
+    _userRepository = UserRepository();
+  }
+
+  @override
+  void dispose() {
+    _authenticationRepository.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return RepositoryProvider.value(
+      value: _authenticationRepository,
+      child: BlocProvider(
+        create: (_) => AuthenticationBloc(
+          authenticationRepository: _authenticationRepository,
+          userRepository: _userRepository,
+        ),
+        child: const AppView(),
+      ),
+    );
+  }
+}
+
+class AppView extends StatefulWidget {
+  const AppView({super.key});
+
+  @override
+  State<AppView> createState() => _AppViewState();
+}
+
+class _AppViewState extends State<AppView> {
+  final _navigatorKey = GlobalKey<NavigatorState>();
+
+  NavigatorState get _navigator => _navigatorKey.currentState!;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // initialRoute: '/',
+      theme: lightTheme(),
+      darkTheme: darkTheme(),
+      navigatorKey: _navigatorKey,
+      debugShowCheckedModeBanner: false,
+      onGenerateRoute: RouteGenerator().generateRoute,
+      builder: (context, child) {
+        return BlocListener<AuthenticationBloc, AuthenticationState>(
+          listener: (context, state) {
+            switch (state.status) {
+              case AuthenticationStatus.authenticated:
+                _navigator.pushAndRemoveUntil<void>(
+                  MyHomePage.route(),
+                  (route) => false,
+                );
+              case AuthenticationStatus.unauthenticated:
+                _navigator.pushAndRemoveUntil<void>(
+                  LoginPage.route(),
+                  (route) => false,
+                );
+              case AuthenticationStatus.unknown:
+                break;
+            }
+          },
+          child: child,
+        );
+      },
+    );
+  }
+}
